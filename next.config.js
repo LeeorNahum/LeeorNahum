24b9afaf1e05fa79cfae34a0b31ac2645252b5e2
LeeorMiner <leeornahum@gmail.com>
2024-12-30 17:04:02 -0500

    Update Next.js configuration to set base path and asset prefix for '/leeornahum.com', enabling trailing slashes for improved URL handling. Remove CNAME file from deployment workflow to streamline the process.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/leeornahum.com',
   images: {
     unoptimized: true,
   },
+  assetPrefix: '/leeornahum.com/',
+  trailingSlash: true,
 }
 
 module.exports = nextConfig 
