d5b0cccbe979e8be648e1209966820cabb345b05
LeeorMiner <leeornahum@gmail.com>
2024-12-30 17:02:23 -0500

    Refactor Next.js configuration to remove base path and asset prefix, simplifying the setup for static export. Update deployment workflow to include copying of CNAME file for proper domain handling during deployment.

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/leeornahum.com',
   images: {
     unoptimized: true,
   },
-  assetPrefix: '/leeornahum.com/',
-  trailingSlash: true,
 }
 
 module.exports = nextConfig 
